f7cdf508d4d02a16094145552410e55fbc3c99c9
outmanwt <812020728@qq.com>
2019-10-23 15:14:59 +0800

    创建第二课

---
+++ course2/myjson.c
@@ -0,0 +1,109 @@
+#include "myjson.h"
+#include <assert.h>
+#include <stdlib.h>
+/*JSON格式
+	JSON-text = ws value ws
+	ws = *(%x20 / %x09 / %x0A / %x0D) 	
+	value = null / false / true 
+	null  = "null"
+	false = "false"
+	true  = "true"
+	
+	当中 %xhh 表示以 16 进制表示的字符，/是多选一，* 是零或多个，( ) 用于分组。
+	那么第一行的意思是，JSON 文本由 3 部分组成，首先是空白（whitespace），接着是一个值，最后是空白。
+	第二行告诉我们，所谓空白，是由零或多个空格符（space U+0020）、制表符（tab U+0009）、换行符（LF U+000A）、回车符（CR U+000D）所组成。
+*/
+/*json结构体*/
+typedef struct {
+    const char* json;
+}json_struct;
+/*解析*/
+json_error json_parse(json_value *v,const char *json);
+json_type json_get_type ( const json_value *v );
+void read_blank_first (json_struct *c );
+json_error json_parse_null(json_struct *c,json_value *v);
+json_error read_value(json_struct *c,json_value *v);
+json_error json_parse_true(json_struct *c,json_value *v);
+json_error json_parse_false(json_struct *c,json_value *v);
+/*解析*/
+json_error json_parse(json_value *v,const char *json)
+{
+	json_struct c;
+	json_error temp;
+	c.json = json;
+	assert(v!=NULL);
+	v->type = JSON_NULL;
+	/*读空格*/
+	read_blank_first(&c);
+	/*读值*/
+	if((temp = read_value(&c,v))==JSON_OK)
+	{
+		read_blank_first(&c);
+		if(*c.json!='\0')
+		{
+			return JSON_INPUT_ERROR;
+		}
+	}	
+
+	return temp;
+}
+/*获取类型*/
+json_type json_get_type ( const json_value *v )
+{
+	assert ( v != NULL );
+	return v->type;
+}
+
+json_error json_parse_null(json_struct *c,json_value *v)
+{
+	/*再检查一遍c是否为n?有必要？*/
+	assert(*c->json=='n');
+	if(c->json[1]!='u'||c->json[2]!='l'||c->json[3]!='l')
+		return JSON_INPUT_ERROR;
+	c->json+=4;
+	v->type = JSON_NULL;
+	return JSON_OK;
+}
+
+json_error json_parse_true(json_struct *c,json_value *v)
+{
+	/*再检查一遍c是否为n?有必要？*/
+	assert(*c->json=='t');
+	if(c->json[1]!='r'||c->json[2]!='u'||c->json[3]!='e')
+		return JSON_INPUT_ERROR;
+	c->json+=4;
+	v->type = JSON_TRUE;
+	return JSON_OK;
+}
+
+json_error json_parse_false(json_struct *c,json_value *v)
+{
+	/*再检查一遍c是否为n?有必要？*/
+	assert(*c->json=='f');
+	if(c->json[1]!='a'||c->json[2]!='l'||c->json[3]!='s'||c->json[4]!='e')
+		return JSON_INPUT_ERROR;
+	c->json+=5;
+	v->type = JSON_FALSE;
+	return JSON_OK;
+}
+
+/*读空格*/
+json_error read_value(json_struct *c,json_value *v)
+{
+	switch(*c->json)
+	{
+		case 'n':	return json_parse_null(c,v);
+		case 't':   return json_parse_true(c,v);
+        case 'f':   return json_parse_false(c,v);
+		case '\0': 	return JSON_ONLY_BLANK;
+		default : 	return JSON_INPUT_ERROR;
+	}
+}
+void read_blank_first (json_struct *c )
+{
+	const char* temp = c->json;
+	while ( *temp == ' ' || *temp == '\t' || *temp == '\n' || *temp == '\r' )
+		temp ++;
+	c->json = temp;
+}
+
---
+++ course2/test.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "myjson.h"
+int static test_count = 0;
+int static err_count = 0;
+int static test_pass = 0;
+static char *str_err[]= {"JSON_OK","JSON_ONLY_BLANK","JSON_INPUT_ERROR","JSON_VALUE_ERROR"};
+static char *str_type[]= {"JSON_NULL","JSON_TRUE","JSON_FALSE"};
+#define EXPECT_EQ(equality, expect, actual, format) \
+    do {\
+        test_count++;\
+        if (equality)\
+            test_pass++;\
+        else {\
+            fprintf(stderr, "%s:%d: \n expect: " format " \n actual: " format "\n", __FILE__, __LINE__, expect,actual );\
+            err_count = 1;\
+        }\
+    } while(0)
+#define EXPECT_EQ_PARSE(expect, actual) EXPECT_EQ((expect) == (actual), str_err[expect], str_err[actual], "%s")
+#define EXPECT_EQ_EQUAL(expect, actual) EXPECT_EQ((expect) == (actual), str_type[expect], str_type[actual], "%s")
+
+static void null_test()
+{
+	json_value v;
+	v.type = JSON_FALSE;
+	/*是否解析成功*/
+	EXPECT_EQ_PARSE(JSON_OK,json_parse(&v,"null"));
+	/*解析类型是否相等*/
+	EXPECT_EQ_EQUAL(JSON_NULL,json_get_type(&v));
+}
+
+static void true_test()
+{
+	json_value v;
+	v.type = JSON_FALSE;
+	/*是否解析成功*/
+	EXPECT_EQ_PARSE(JSON_OK,json_parse(&v,"true"));
+	/*解析类型是否相等*/
+	EXPECT_EQ_EQUAL(JSON_TRUE,json_get_type(&v));
+
+	/*是否解析成功*/
+	EXPECT_EQ_PARSE(JSON_OK,json_parse(&v," true"));
+	/*解析类型是否相等*/
+	EXPECT_EQ_EQUAL(JSON_TRUE,json_get_type(&v));
+}
+
+static void false_test()
+{
+	json_value v;
+	v.type = JSON_FALSE;
+	/*是否解析成功*/
+	EXPECT_EQ_PARSE(JSON_OK,json_parse(&v,"false"));
+	/*解析类型是否相等*/
+	EXPECT_EQ_EQUAL(JSON_FALSE,json_get_type(&v));
+
+	/*是否解析成功*/
+	EXPECT_EQ_PARSE(JSON_OK,json_parse(&v," false"));
+	/*解析类型是否相等*/
+	EXPECT_EQ_EQUAL(JSON_FALSE,json_get_type(&v));
+}
+static void empty_test()
+{
+	json_value v;
+	v.type = JSON_FALSE;
+	/*是否解析成功*/
+	EXPECT_EQ_PARSE(JSON_ONLY_BLANK,json_parse(&v,""));
+	/*解析类型是否相等*/
+	EXPECT_EQ_EQUAL(JSON_NULL,json_get_type(&v));
+
+	/*是否解析成功*/
+	EXPECT_EQ_PARSE(JSON_ONLY_BLANK,json_parse(&v," "));
+	/*解析类型是否相等*/
+	EXPECT_EQ_EQUAL(JSON_NULL,json_get_type(&v));
+}
+
+static void invalid_input_test()
+{
+	json_value v;
+	v.type = JSON_FALSE;
+	/*是否解析成功*/
+	EXPECT_EQ_PARSE(JSON_INPUT_ERROR,json_parse(&v,"?"));
+	/*解析类型是否相等*/
+	EXPECT_EQ_EQUAL(JSON_NULL,json_get_type(&v));
+
+	/*是否解析成功*/
+	EXPECT_EQ_PARSE(JSON_INPUT_ERROR,json_parse(&v," true?"));
+	/*解析类型是否相等*/
+	EXPECT_EQ_EQUAL(JSON_TRUE,json_get_type(&v));
+}
+static void parse_test()
+{
+	null_test();
+	true_test();
+	false_test();
+	empty_test();
+	invalid_input_test();
+}
+int main() 
+{
+/*
+	json_value v;
+	char *str[]= {"JSON_OK","JSON_ONLY_BLANK","JSON_INPUT_ERROR","JSON_VALUE_ERROR"};
+	v.type = JSON_FALSE;
+	fprintf ( stderr , "%s:%s\n", "",str[json_parse ( &v , "" )]); 
+	fprintf ( stderr , "%s:%s\n", " ", str[json_parse ( &v , " " )]); 
+	fprintf ( stderr , "%s:%s\n", "Null",str[json_parse ( &v , "Null" )]); 
+	fprintf ( stderr , "%s:%s\n", "null",str[json_parse ( &v , "null" )]); 
+	fprintf ( stderr , "%s:%s\n", " null",str[json_parse ( &v , " null" )]); 
+	fprintf ( stderr , "%s:%s\n", "nullx",str[json_parse ( &v , "nullx" )]); 
+	fprintf ( stderr , "%s:%s\n", "ac",str[json_parse ( &v , "ac" )]); 
+	fprintf ( stderr , "%s:%s\n", "true",str[json_parse ( &v , "true" )]); 
+	fprintf ( stderr , "%s:%s\n", "false",str[json_parse ( &v , "false" )]); 
+*/
+	parse_test();
+	printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+	return 0;
+}
